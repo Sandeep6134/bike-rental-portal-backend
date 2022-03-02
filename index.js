40c223ee7bcaaa00e1f0003f0f3e68975f3850c8
Sandeep Nagarajan <sandeepnagarajan@AGLS-MLT-443.local>
2022-03-02 16:09:27 +0530

    trying

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ mongo.connect();
 app.get("/bike",async(req,res,next)=>{
         res.send("server")
 })
-app.post("/adminsignin",userSignin)
-app.post("/adminsignup",userSignup)
+app.post("/usersignin",userSignin)
+app.post("/usersignup",userSignup)
 app.get("/users",get)
 app.post("/update/booking",userUpdateBooking)
 app.get("/bike/list",async(req,res,next)=>{
 
     try {
-        var data = await mongo.db.collection("Bikes").find({booking:1}).toArray();
+        var data = await mongo.db.collection("Bikes").find().toArray();
         res.send(data);
     } catch(err) {
         console.log(err);
